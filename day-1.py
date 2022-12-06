de7d69513fe07225ee9c9baef63606b83ed45b4b
Jonah Marz <jmarz793@yahoo.com>
2022-12-05 17:11:15 -0800

    First day

---
+++ day-1.py
@@ -0,0 +1,20 @@
+
+
+# Reads the file for the next elf
+def read_in_elf():
+    filepath = './inputs/day-1.txt'
+    calories_total = 0
+    with open(filepath) as fp:
+        line = fp.readline()
+        while line:
+            if line == '\n':
+                yield calories_total
+                calories_total = 0
+            else:
+                calories_total+=int(line)
+            line = fp.readline()
+
+max_calories = 0
+for calories in read_in_elf():
+    max_calories = max(max_calories, calories)
+print(max_calories)
