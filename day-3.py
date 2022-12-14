ab06b576be831938f51e2cd4f1de5511472d5f48
Jonah Marz <jmarz793@yahoo.com>
2022-12-14 14:24:46 -0800

    day-3 part 2

--- day-3.py
+++ day-3.py
@@ -6,14 +6,19 @@ letter_to_priority = {alphabet[i] : i+1 for i in range(len(alphabet))}
 FILE_PATH = './inputs/day-3.txt'
 
 # find shared element and return priority
-def find_shared_element(line):
-  compartment_1, compartment_2 = line[:len(line)//2], line[len(line)//2:]
-  intersection = list(set(compartment_1) & set(compartment_2))
+def find_shared_element(line1, line2, line3):
+  intersection = list(set(line1) & set(line2) & set(line3))
+  intersection.remove('\n')
   print(intersection)
   return letter_to_priority[intersection[0]]
     
 summ = 0
-for line in parse_file(FILE_PATH):
-    summ += find_shared_element(line)
+lines = [line for line in parse_file(FILE_PATH)]
+three_lines = []
+for i in range(len(lines)):
+    three_lines.append(lines[i])
+    if len(three_lines) == 3:
+        summ+=find_shared_element(three_lines[0], three_lines[1], three_lines[2])
+        three_lines = []
 print(summ)
     
